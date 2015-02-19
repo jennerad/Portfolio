d52e65b4280001dd4e77a6df2ece7c3fb7a903a2
Jennifer Nerad <jnerad@gmail.com>
2015-02-18 17:12:57 -0800

    updated with smooth scrolling and mockups

--- js/main.js
+++ js/main.js
@@ -63,3 +63,21 @@ $('h1').mouseenter(function(){
     prepareHeadline($headline);
     animateHeadline($headline, 50);
 });
+
+
+//smooth-scrolling via http://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
